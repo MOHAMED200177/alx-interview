6d6f69f17902fd1f8c29e734b548eb0cf4e5fdb5
Moahmed Elafandy <mohamedelafandy593@gmail.com>
2024-04-24 22:53:51 +0000

    solve

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-""""
-Minimum Operations
-"""
+""" Minimum Operations
+    """
 
 
 def minOperations(n: int) -> int:
     """ Minimum Operations needed to get n H characters """
-    past = "H"
-    text = "H"
-    Operations = 0
-    while (len(text) < n):
-        if n % len(text) == 0:
-            Operations += 2
-            past = text
-            text += text
-        else :
-            Operations += 1
-            text += past
-    if len(text) != n:
+    next = 'H'
+    body = 'H'
+    op = 0
+    while (len(body) < n):
+        if n % len(body) == 0:
+            op += 2
+            next = body
+            body += body
+        else:
+            op += 1
+            body += next
+    if len(body) != n:
         return 0
-    return Operations
+    return op
