509617084afb20bbda1825bb8896d33db759298c
Moahmed Elafandy <mohamedelafandy593@gmail.com>
2024-04-24 22:44:50 +0000

    solve

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/user/bin/python3
+""""Minimum Operations"""
+def minOperations(n):
+    """ Minimum Operations needed to get n H characters """
+    past = "H"
+    text = "H"
+    Operations = 0
+
+    while(len(text) < n):
+        if(n % len(text) == 0):
+            Operations += 2
+            past = text
+            text += text
+        else :
+            Operations += 1
+            text += past
+    if len(text) != n :
+        return 0
+    return Operations
