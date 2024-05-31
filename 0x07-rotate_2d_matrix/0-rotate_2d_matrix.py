1ebcb996cd17a7ee9628e17215fa0f81840cc05e
Mohamed Elafandy <mohamedelafandy593@gmail.com>
2024-05-31 23:03:20 +0000

    Rotate 2D Matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-""" Rotate 2D Matrix
-"""
+"""Rotates 2-D matrix:"""
 
 
+def rotate_2d_matrix(m):
+    """Rotates in-place"""
+    n = len(m)
+    temp1, temp2 = 0, 0
 
-def rotate_2d_matrix(matrix):
-    """ Given an n x n 2D matrix, rotate it 90 degrees clockwise
-    """
-    newMatrix = zip(*reversed(matrix))
-    for n, x in enumerate(newMatrix):
-        matrix[n] = list(x)
-    
-
-
-if __name__ == '__main__':
-    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-
-    """ rotate_2d_matrix(matrix) """
-    rotate_2d_matrix(matrix)
-    print(matrix)
+    for j in range(0, len(m) // 2 + 1):
+        for i in range(j, n - 1):
+            # For r in first row, put in same position in col from back
+            temp1 = m[i][n - 1]
+            m[i][n - 1] = m[j][i]
+            # put that temp1 in reverse position in row from bottom
+            temp2 = m[n - 1][n - 1 - i + j]
+            m[n - 1][n - 1 - i + j] = temp1
+            # put that temp2 in same position in col from front
+            temp1 = m[n - 1 - i + j][j]
+            m[n - 1 - i + j][j] = temp2
+            # put that temp1 in reverse position in row from top
+            m[j][i] = temp1
+        n -= 1
